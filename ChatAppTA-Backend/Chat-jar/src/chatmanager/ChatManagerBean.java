05f7f769b253b5d2a3e687927b1b43c7c7991edd
Boris Horvat <horvat.boris99@gmail.com>
2022-05-23 10:35:43 +0200

    feat: chat app front and back begin

---
+++ ChatAppTA-Backend/Chat-jar/src/chatmanager/ChatManagerBean.java
@@ -0,0 +1,56 @@
+package chatmanager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateful;
+
+import models.User;
+
+// TODO Implement the rest of Client-Server functionalities 
+/**
+ * Session Bean implementation class ChatBean
+ */
+@Stateful
+@LocalBean
+public class ChatManagerBean implements ChatManagerRemote, ChatManagerLocal {
+
+	private List<User> registered = new ArrayList<User>();
+	private List<User> loggedIn = new ArrayList<User>();
+	
+	/**
+	 * Default constructor.
+	 */
+	public ChatManagerBean() {
+	}
+
+	@Override
+	public boolean register(User user) {
+		boolean exists = registered.stream().anyMatch(u->u.getUsername().equals(user.getUsername()));
+		if(exists) {
+			return false;
+		}
+		registered.add(user);
+		return true;
+	}
+
+	@Override
+	public boolean login(String username, String password) {
+		boolean exists = registered.stream().anyMatch(u->u.getUsername().equals(username) && u.getPassword().equals(password));
+		if(exists)
+			loggedIn.add(new User(username, password));
+		return exists;
+	}
+
+	@Override
+	public List<User> loggedInUsers() {
+		return loggedIn;
+	}
+
+	@Override
+	public boolean logOut(User user) {
+		 return loggedIn.remove(user);
+	}
+
+}
---
+++ ChatAppTA-Backend/Chat-jar/src/chatmanager/ChatManagerRemote.java
@@ -0,0 +1,21 @@
+package chatmanager;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import models.User;
+
+@Remote
+public interface ChatManagerRemote {
+
+	public boolean login(String username, String password);
+
+	public boolean register(User user);
+
+	public List<User> loggedInUsers();
+	
+	public boolean logOut(User user);
+	
+	
+}
